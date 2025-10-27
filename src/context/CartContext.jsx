b813f0eb9d997136efff86dad1c4dbbb5922daab
sofja <sofi.efron@gmail.com>
2025-10-27 18:46:29 -0300

    Create CartContext

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems((prev) => [...prev, product]);
+    console.log(cartItems);
+  };
+
+  const removeFromCart = (productId) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== productId));
+  };
+
+  const clearCart = () => setCartItems([]);
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <CartProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </CartProvider>
   </BrowserRouter>
 );
