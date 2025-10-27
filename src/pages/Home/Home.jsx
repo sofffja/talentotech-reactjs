2c7973e8585967bbcff41d6da28f5e317f30ee00
sofja <sofi.efron@gmail.com>
2025-10-27 18:48:06 -0300

    Update Home layout and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import './Home.css';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router';
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleExploreClick = () => {
+    navigate('/productos');
+  };
+
   return (
     <div className="home">
-      <h1>Inicio</h1>
+      <h2>Bienvenidxs a la tienda</h2>
+      <p>Encuentra los mejores productos</p>
+      <div className="home__button">
+        <Button onClick={handleExploreClick}>Explorar</Button>
+      </div>
     </div>
   );
 }
