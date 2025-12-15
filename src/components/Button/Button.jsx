5e3b0a840a8b793341c42c77fbb9b0eda5c70345
sofffja <sofi.efron@gmail.com>
2025-12-15 13:34:08 +0000

    Enhance Button component with disabled state and type prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,19 @@
 import "./Button.css";
 
-export default function Button({ children, onClick, variant = "default" }) {
+export default function Button({
+  children,
+  onClick,
+  variant = "default",
+  disabled = false,
+  type = "button",
+}) {
   return (
-    <button onClick={onClick} className={`button button--${variant}`}>
+    <button
+      onClick={onClick}
+      className={`button button--${variant}`}
+      disabled={disabled}
+      type={type}
+    >
       {children}
     </button>
   );
