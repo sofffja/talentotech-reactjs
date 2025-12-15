77e8625c7c17168d292c7b5aaeb761a7b35d6542
sofffja <sofi.efron@gmail.com>
2025-12-15 05:12:33 +0000

    Add mobile menu and icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,69 @@
-import './Header.css';
-import { NavLink } from 'react-router';
-import { useContext } from 'react';
-import { CartContext } from '../../context/CartContext';
+import "./Header.css";
+import { NavLink } from "react-router";
+import { useAuthContext } from "../../context/AuthContext";
+import { useCartContext } from "../../context/CartContext";
+import Button from "../Button/Button";
+import { FaShoppingCart, FaUser, FaSignInAlt, FaSignOutAlt, FaHome, FaStore, FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Header() {
-  const { cartItems } = useContext(CartContext);
+  const { isAuthenticated, logout } = useAuthContext();
+  const { cartItems } = useCartContext();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <div className="header">
-      <h1>Tienda</h1>
-      <nav>
+    <header className="header">
+      <div className="header__logo">
+        <h1>Tienda</h1>
+      </div>
+      
+      <button className="header__toggle" onClick={toggleMenu}>
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+
+      <nav className={`header__nav ${isMenuOpen ? "header__nav--open" : ""}`}>
         <ul className="header__menu">
           <li>
-            <NavLink to="/">Inicio</NavLink>
+            <NavLink to="/" onClick={() => setIsMenuOpen(false)}>
+                Inicio
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/productos">Productos</NavLink>
+            <NavLink to="/productos" onClick={() => setIsMenuOpen(false)}>
+               Productos
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/carrito" className="header__cart">
+            <NavLink to="/carrito" className="header__cart" onClick={() => setIsMenuOpen(false)}>
+              <FaShoppingCart />
               <span>Carrito</span>
               <span className="header__count">{cartItems.length}</span>
             </NavLink>
           </li>
+          {isAuthenticated ? (
+            <>
+              <li className="header__admin">
+                <NavLink to="/admin" onClick={() => setIsMenuOpen(false)}>
+                    Admin
+                </NavLink>
+              </li>
+              <li>
+                <Button onClick={() => { logout(); setIsMenuOpen(false); }}>
+                    <FaSignOutAlt /> LOG OUT
+                </Button>
+              </li>
+            </>
+          ) : (
+            <li>
+              <NavLink to="/login" onClick={() => setIsMenuOpen(false)}>
+                <FaSignInAlt /> Log in
+              </NavLink>
+            </li>
+          )}
         </ul>
       </nav>
-    </div>
+    </header>
   );
 }
